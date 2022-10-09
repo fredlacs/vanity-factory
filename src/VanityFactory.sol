0260f554146fdc6349bf88ce07310f94e5aec594
fredlacs <32464905+fredlacs@users.noreply.github.com>
2022-10-09 04:14:20 +0100

    fix smol tings

--- src/VanityFactory.sol
+++ src/VanityFactory.sol
@@ -34,7 +34,7 @@ contract VanityFactory {
     function ask(Context calldata ctx) external payable returns (uint256 id) {
         require(ctx.reward > 0, "VanityFactory: no reward set");
         require(msg.value >= ctx.reward, "VanityFactory: not enough reward sent");
-        require(ctx.salt == bytes32(bytes20(msg.sender)), "VanityFactory: initial salt not zero");
+        require(ctx.salt == bytes32(bytes20(msg.sender)), "VanityFactory: initial salt not address");
         id = deploys.length;
         deploys.push(ctx);
         emit Asked(id, ctx.scorer, ctx.initCodeHash, ctx.salt, ctx.minScoreWinner, ctx.endTime, ctx.reward);
@@ -45,7 +45,7 @@ contract VanityFactory {
         require(block.timestamp < ctx.endTime, "VanityFactory: bid time ended");
         address minedAddress = Create2.computeAddress(salt, ctx.initCodeHash);
         uint256 score = ctx.scorer.score(minedAddress);
-        require(score >= ctx.minScoreWinner, "VanityFactory: not high enough score");
+        require(score > ctx.minScoreWinner, "VanityFactory: not high enough score");
 
         ctx.minScoreWinner = score;
         ctx.salt = salt;
